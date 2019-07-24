57f529af66757b9b5e61680868bf6425be38a5a2
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-07-24 14:12:00 -0700

    added basic jquery to the app

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-
+$(function(){
 
 $('#select-menu').on('change', function() {
     const selected = $(this).val();
     if (selected !== '') {
       console.log('The value you picked is: ' + selected);
+      loadArticles(selected);
     }
   });
+
+  function loadArticles(selected){
+
+    $.ajax({
+      method: 'get',
+      url:'https://api.nytimes.com/svc/topstories/v2/'+selected+'.json?api-key=aH4FAhLkiG9ICtGxYuBVuGhjQviN4Geb'
+    }).done(function(data){
+      console.log(data.results);
+      const results = data.results;
+      //append data
+      // try template strings``
+      //$.each(results, function(index,value){
+      //append an article template `<article><p>${abstract}</p></article>
+     // })
+      
+    }).fail().always();
+
+  }
+});//end of document ready
